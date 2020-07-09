bf953dd3b945c8b2aeeb069d077991542be0fe23
jcruz97 <cruzjurgen97@gmail.com>
2020-07-09 16:04:43 +0200

    06 in 02-maths

--- 01.Series-1/02-maths/06-fizzbuzz/script.js
+++ 01.Series-1/02-maths/06-fizzbuzz/script.js
@@ -2,7 +2,24 @@
 // 02-maths/06-fizzbuzz/script.js - 2.6: fizzbuzz
 
 (() => {
+    
+    for(let i=1;i<=100;i++){
 
-    // your code here
+        if(i%3===0 && i%5===0){
+            
+            console.log("fizzbuzz");
+
+        } else if ((i%5)===0){
+
+            console.log("buzz");
+
+        } else if ((i%3)===0){
+
+            console.log("fizz");
+
+        } else {
+            console.log(i);
+        }
+    }
 
 })();
