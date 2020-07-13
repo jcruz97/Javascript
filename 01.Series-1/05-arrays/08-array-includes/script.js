f36e8a36365591e5f9d45b28cdf46b1cf02f593d
jcruz97 <cruzjurgen97@gmail.com>
2020-07-13 15:25:53 +0200

    08/09 in 05-arrays

--- 01.Series-1/05-arrays/08-array-includes/script.js
+++ 01.Series-1/05-arrays/08-array-includes/script.js
@@ -17,6 +17,18 @@
         "cherry",
     ];
 
-    // your code here
+    document.getElementById("run").addEventListener("click", () =>{
+
+
+        let finder = fruits.includes("apple");
+
+        if (finder==true){
+            console.log("There is an apple in the array :)");
+        } else {
+            console.log("There isn't any apple in the array sorry :(");
+        }
+        
+
+    });
 
 })();
--- 01.Series-1/05-arrays/09-rand-array-stats/script.js
+++ 01.Series-1/05-arrays/09-rand-array-stats/script.js
@@ -1,7 +1,32 @@
 // 05-arrays/09-rand-array-stats/script.js - 5.9: random & statistics array
-
+//document.getElementById("element-id").innerHTML = "new-value"
 (() => {
 
-    // your code here
+    document.getElementById("run").addEventListener("click", () =>{
+
+        let numberArr=[];
+
+        for (let i=0;i<10;i++){
+
+           numberArr[i]= Math.floor(Math.random() * 101);
+           document.getElementById('n-' + (i+1)).innerHTML = numberArr[i];
+
+            min = Math.min(...numberArr);
+            max = Math.max(...numberArr);
+            sum = numberArr.reduce((a, b) => a + b, 0);
+            avg = (sum/(numberArr.length));
+
+        }
+
+        document.getElementById("min").innerHTML = min;
+        document.getElementById("max").innerHTML = max;
+        document.getElementById("sum").innerHTML = sum;
+        document.getElementById("average").innerHTML = avg;
+
+        
+        
+
+        
+    });
 
 })();
